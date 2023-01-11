d75c46cf6710fbd6cfba5dce18114b7ac3ae1114
aldirif <97742642+AldiRif@users.noreply.github.com>
2023-01-11 16:36:01 +0700

    Add project files.

---
+++ POS.DataContext/Migrations/20230111083621_AddToDatabase.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POS.Repository.Migrations
+{
+    public partial class AddToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tbl_category",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    category_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_category", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_customers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    company_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contact_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contact_title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    region = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    postal_code = table.Column<int>(type: "int", nullable: false),
+                    country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    fax = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_customers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    title_of_courtesy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    birth_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    hire_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    region = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    postal_code = table.Column<int>(type: "int", nullable: false),
+                    country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    home_phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    extension = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    notes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    reports_to = table.Column<int>(type: "int", nullable: false),
+                    photo_path = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_employees", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_supplier",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    company_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contact_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contact_title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    region = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    postal_code = table.Column<int>(type: "int", nullable: false),
+                    country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    fax = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    home_page = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_supplier", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_order",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomersId = table.Column<int>(type: "int", nullable: false),
+                    EmployeesId = table.Column<int>(type: "int", nullable: false),
+                    order_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    required_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    shipped_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ship_via = table.Column<int>(type: "int", nullable: false),
+                    freight = table.Column<int>(type: "int", nullable: false),
+                    ship_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ship_address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ship_city = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ship_region = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ship_postal_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ship_country = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_order", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_tbl_order_tbl_customers_CustomersId",
+                        column: x => x.CustomersId,
+                        principalTable: "tbl_customers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tbl_order_tbl_employees_EmployeesId",
+                        column: x => x.EmployeesId,
+                        principalTable: "tbl_employees",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    product_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SupplierId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    quantity_per_unit = table.Column<long>(type: "bigint", nullable: false),
+                    unit_price = table.Column<double>(type: "float", nullable: false),
+                    unit_in_stock = table.Column<long>(type: "bigint", nullable: false),
+                    unit_on_order = table.Column<long>(type: "bigint", nullable: false),
+                    reorder_level = table.Column<long>(type: "bigint", nullable: false),
+                    discontinued = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_product", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_tbl_product_tbl_category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "tbl_category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tbl_product_tbl_supplier_SupplierId",
+                        column: x => x.SupplierId,
+                        principalTable: "tbl_supplier",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_order_details",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrdersId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    unit_price = table.Column<int>(type: "int", nullable: false),
+                    quantity = table.Column<long>(type: "bigint", nullable: false),
+                    discount = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_order_details", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_tbl_order_details_tbl_order_OrdersId",
+                        column: x => x.OrdersId,
+                        principalTable: "tbl_order",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tbl_order_details_tbl_product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "tbl_product",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_order_CustomersId",
+                table: "tbl_order",
+                column: "CustomersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_order_EmployeesId",
+                table: "tbl_order",
+                column: "EmployeesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_order_details_OrdersId",
+                table: "tbl_order_details",
+                column: "OrdersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_order_details_ProductId",
+                table: "tbl_order_details",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_product_CategoryId",
+                table: "tbl_product",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_product_SupplierId",
+                table: "tbl_product",
+                column: "SupplierId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tbl_order_details");
+
+            migrationBuilder.DropTable(
+                name: "tbl_order");
+
+            migrationBuilder.DropTable(
+                name: "tbl_product");
+
+            migrationBuilder.DropTable(
+                name: "tbl_customers");
+
+            migrationBuilder.DropTable(
+                name: "tbl_employees");
+
+            migrationBuilder.DropTable(
+                name: "tbl_category");
+
+            migrationBuilder.DropTable(
+                name: "tbl_supplier");
+        }
+    }
+}
---
+++ POS.Service/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using POS.Repository;
+
+namespace POS.Service
+{
+    public class CategoryService
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<CategoryEntity> GetCategories()
+        {
+            return _context.categoryEntities.ToList();
+        }
+
+        public CategoryEntity Add(CategoryEntity category)
+        {
+            try
+            {
+                _context.categoryEntities.Add(category);
+                _context.SaveChanges();
+                return category;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
